92fb463f0ab14480f28008017ba6f89f3fbfca43
Daniel Torres <nobeeakon@gmail.com>
2024-05-29 18:45:38 -0600

    1) tmp: monitoring api, 2) minor cleanup

---
+++ src/routes/api/stats/monitoring.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import MonitoringRequestModel from '../../../db/models/MonitoringRequestModel';
+
+const router = Router();
+
+// TODO: make something better
+router.get('/monitoring', async (_req, res) => {
+    try {
+        const monitoringData = await MonitoringRequestModel.get();
+
+        return res.json(monitoringData);
+    } catch (error) {
+        return res.json({});
+    }
+});
+
+export default router;
